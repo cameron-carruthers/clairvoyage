8a048e308cf6ebb6b11b34e4d855bf1e7dfe2418
Cameron Carruthers <cameronlcarruthers@gmail.com>
2021-09-19 19:13:15 -0600

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
 }
-export default MyApp
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+)
+
+export default MyApp;
